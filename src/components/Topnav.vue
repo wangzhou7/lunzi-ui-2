5322d8266ff2dadfda233fcaf4a8f4c595f25b66
wangzhou7 <793856657@qq.com>
2024-04-15 15:02:34 +0800

    让 topnav 是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
-    <ul class="menus">
+    <div class="logo" @click='toggleMenu'>LOGO</div>
+    <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside"></span>
   </div>
 </template>
 
@@ -33,6 +34,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
 
   >.logo {
     max-width: 6em;
@@ -48,5 +51,11 @@ export default {
       margin: 0 1em;
     }
   }
+  > .toggleAside{
+  }
+  @media(max-width:500px){
+  > .menu{display: none;}
+  > .logo{margin: 0 auto;}
+}
 }
 </style>
