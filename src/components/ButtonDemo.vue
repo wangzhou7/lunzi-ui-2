a0c00ef389ad9534e4c262186f826095aec4a9da
wangzhou7 <793856657@qq.com>
2024-04-16 15:09:19 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,9 +1,18 @@
 <template>
+    <div>Button 示例 </div>
+    <h1>示例1</h1>
     <div>
-       Button 示例 
+        <Button>你好</Button>
     </div>
 </template>
 
 
 <style lang="scss">
 </style>
+
+<script lang="ts">
+import Button from '../lib/Button.vue';
+export default {
+    components:{ Button }
+}
+</script>
