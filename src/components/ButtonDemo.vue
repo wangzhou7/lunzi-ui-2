2aef2a3829f311c0f97674755d838de303a2591b
wangzhou7 <793856657@qq.com>
2024-04-16 16:59:25 +0800

    支持 CSS2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -13,6 +13,16 @@
         <Button>普普普</Button>
       <Button size="small">小小小</Button>
     </div>
+    <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普普</Button>
+        <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div>
+        <Button theme="text" size="big">大大大</Button>
+        <Button theme="text">普普普</Button>
+        <Button theme="text" size="small">小小小</Button>
+    </div>
 </template>
 
 
