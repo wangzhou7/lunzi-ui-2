aed83cd524a944693a96e295123e32fc0c780f56
wangzhou7 <793856657@qq.com>
2024-04-16 20:47:03 +0800

    基本完善 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,13 +1,29 @@
 <template>
     <div>Button 示例 </div>
-    <h1>示例1</h1>
+    <h2>示例1</h2>
     <div>
         <Button>你好</Button>
         <Button theme="button">你好</Button>
         <Button theme="link">你好</Button>
         <Button theme="text">你好</Button>
     </div>
-    <h1>示例2</h1>
+    <h2>示例2</h2>
+    <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
+    <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <h2>示例3</h2>
     <div>
         <Button size="big">大大大</Button>
         <Button>普普普</Button>
@@ -22,6 +38,19 @@
         <Button theme="text" size="big">大大大</Button>
         <Button theme="text">普普普</Button>
         <Button theme="text" size="small">小小小</Button>
+    </div>
+   
+    <h2>示例4</h2>
+    <div>
+      <Button disabled> 禁用按钮 </Button>
+      <Button :disabled="false"> 禁用按钮 </Button>
+      <Button theme="link" disabled>禁用链接按钮</Button>
+      <Button theme="text" disabled>禁用按钮</Button>
+    </div>
+    <h2>示例5</h2>
+      <div>
+      <Button loading> 加载中 </Button>
+      <Button> 加载完毕 </Button>
     </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="lunzi-button" :class="classes">
+    <button class="lunzi-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="lunzi-loadingIndicator"></span>
         <slot />
     </button>
 </template>
@@ -17,13 +18,26 @@ export default {
             type: String,
             default: "normal"
         },
+        level: {
+            type: String,
+            default: "normal"
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        loading:{
+            type: Boolean,
+            default: false
+        }
     },
     setup(props) {
-        const { theme, size } = props
+        const { theme, size, level } = props
         const classes = computed(() => {
             return {
                 [`lunzi-theme-${theme}`]: theme,
                 [`lunzi-size-${size}`]: size,
+                [`lunzi-level-${level}`]: level,
             };
         });
         return { classes };
@@ -37,6 +51,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 
 .lunzi-button {
     box-sizing: border-box;
@@ -52,8 +68,9 @@ $radius: 4px;
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
 
-    &+& {
+    & + & {
         margin-left: 8px;
     }
 
@@ -105,5 +122,102 @@ $radius: 4px;
         padding: 0 4px;
     }
 
+    &.lunzi-theme-button {
+        &.lunzi-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+
+        &.lunzi-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.lunzi-theme-link {
+        &.lunzi-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.lunzi-theme-text {
+        &.lunzi-level-main {
+            color: $blue;
+
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+
+        &.lunzi-level-danger {
+            color: $red;
+
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+
+    &.lunzi-theme-button {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+
+            &:hover {
+                border-color: $grey;
+            }
+        }
+    }
+
+    &.lunzi-theme-link,
+    &.lunzi-theme-text {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
+        }
+    }
+
+    > .lunzi-loadingIndicator {
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        margin-right: 4px;
+        border-radius: 8px;
+        border-color: $blue $blue $blue transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: lunzi-spin 1s infinite linear;
+    }
+}
+
+@keyframes lunzi-spin {
+    0% {
+        transform: rotate(0deg)
+    }
+
+    100% {
+        transform: rotate(360deg)
+    }
 }
 </style>
