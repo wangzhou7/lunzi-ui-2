9c7710a8d7d789c03fa5ae7b9d971c81196f1b93
wangzhou7 <793856657@qq.com>
2024-04-16 15:27:12 +0800

    创建 Button 组件2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,17 +2,27 @@
     <div>Button 示例 </div>
     <h1>示例1</h1>
     <div>
-        <Button>你好</Button>
+        <Button @click="onclick"
+                @foucs="onclick"
+                @mouseover="onclick"
+                size="small">
+                你好</Button>
     </div>
 </template>
 
 
-<style lang="scss">
-</style>
-
 <script lang="ts">
 import Button from '../lib/Button.vue';
 export default {
-    components:{ Button }
+    components:{ Button },
+    setup(){
+    const onClick = ()=>{
+      console.log('hi')
+    }
+    return {onClick}
+  }
 }
 </script>
+
+<style lang="scss">
+</style>
