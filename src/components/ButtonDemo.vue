314e2c31ae8e200a1062ff27cc4d24c7e148f11d
wangzhou7 <793856657@qq.com>
2024-04-20 21:41:55 +0800

    实现下划线滑动

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -43,9 +43,8 @@
     <h2>示例4</h2>
     <div>
       <Button disabled> 禁用按钮 </Button>
-      <Button :disabled="false"> 禁用按钮 </Button>
       <Button theme="link" disabled>禁用链接按钮</Button>
-      <Button theme="text" disabled>禁用按钮</Button>
+      <Button theme="text" disabled>禁用文字按钮</Button>
     </div>
     <h2>示例5</h2>
       <div>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,21 +2,26 @@
     <div>
         Tabs 示例
         <h2>示例1</h2>
-        <Tabs selected="导航1">
+        <Tabs v-model:selected="x">
             <Tab title="导航1"> 内容1 </Tab>
-            <Tab title="导航2"> 内容2</Tab>
+            <Tab title="导航222222"> 内容2 </Tab>
         </Tabs>
     </div>
 </template>
 
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
-import Tab from '../lib/tab.vue';
+import Tab from '../lib/Tab.vue'
+import { ref } from 'vue';
 export default {
     components: {
         Tabs,
         Tab
-    }
+    },
+    setup() {
+        const x = ref('导航1')
+        return { x }
+    },
 }
 </script>
 
